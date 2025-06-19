469394091a72f8ca06f4c951b2045de7250a2e9a
Stephen Tse <stephentse@microsoft.com>
2025-06-19 16:22:10 -0700

    Added comment on concurrency best practice

--- src/dependency.rs
+++ src/dependency.rs
@@ -5,6 +5,12 @@ use crate::repo::db::{InMemoryDatabase, KVDatabase};
 /// Application state that holds all the app dependency singletons.
 #[derive(Clone)]
 pub struct ApplicationState {
+    // Note: Locking database client with `Arc<Mutex>` or `Arc<RwLock>` is not ideal for throughput,
+    //   it means we can only issue one operation at a time.
+    //   In practice, make sure that the database client is either one of the followings:
+    //   - Bitwise copyable, i.e. it only clones pointers to the connection pool.
+    //   - Allows you to get a pointer to the shared underlying resource with e.g. `get_ref()` or `get_mut()`.
+    // Library documentation typically states this clearly.
     pub db: Arc<RwLock<dyn KVDatabase<String, String>>>,
 }
 
